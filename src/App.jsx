ae444eb44db5daefb36efae92579f52d162be735
topilovasilbek <asilbektback@gmail.com>
2023-03-09 18:25:45 +0500

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Root from "./02-03/root.jsx";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Root />
+      </>
+    );
+  }
+}
+
+export default App;
